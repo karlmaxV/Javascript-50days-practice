4308bc99eadcd9f18cc6374b4144e4ac4e5c1062
Victor Chaman <98865008+karlmaxV@users.noreply.github.com>
2022-08-16 19:16:31 +0100

    Add files via upload

---
+++ scrollboxes.js
@@ -0,0 +1,18 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkboxes)
+checkboxes()
+function checkboxes(){
+    const triggerBottom = window.innerHeight / 3 * 2
+
+    boxes.forEach(box =>{
+        const boxTop=box.getBoundingClientRect().top
+        if(boxTop < triggerBottom){
+            box.classList.add('show')
+        }else{
+            box.classList.remove('show')
+        }
+    })
+
+    
+}
